03eb74047805086adf6f9a7cc8239325ece3ade5
Pedro Posada <p_posada@straight.com>
2012-08-20 17:51:38 -0700

    initial commit to pp

---
+++ sites/default/local.settings.php
@@ -0,0 +1,10 @@
+<?php
+
+// add D6 database connection for acquia environment only
+$databases['d6']['default'] = array(
+	'driver' => 'mysql',
+	'database' => 'ppd6',
+	'username' => 'root',
+	'password' => 'P@ssw0rd',
+	'host' => 'localhost',
+);
