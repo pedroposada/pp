8888eb30ef939bf07755e9d25beccd67140c442a
Pedro Posada <pedro@pedro-ThinkPad-R61.(none)>
2012-11-16 18:20:46 -0500

    modules and styles

--- sites/all/themes/sky/js/sky.js
+++ sites/all/themes/sky/js/sky.js
@@ -15,8 +15,20 @@
 			});
 			
 			// make proper tab active in node page
-			window.location.pathname.match(/^\/portfolio\//) && $('li.menu-item-1685').addClass('active-trail').find('a').addClass('active-trail active');
-			window.location.pathname.match(/^\/blog\//) && $('li.menu-item-719').addClass('active-trail').find('a').addClass('active-trail active');
+			var path = window.location.pathname;
+			$('#block-system-main-menu li').each(function(i){
+				var src = $('a', this).attr('href');
+				if (src.length > 1) {
+					src = src.replace(/\//g,"\\/");
+					var pat = new RegExp('^' + src);
+					console.log(pat);
+					path.match(pat) && $(this).addClass('active-trail').find('a').addClass('active-trail active');
+				};
+			});
+			// path.match(/^\/portfolio\//) && $('li.menu-item-1685').addClass('active-trail').find('a').addClass('active-trail active');
+			// path.match(/^\/blog\//) && $('li.menu-item-719').addClass('active-trail').find('a').addClass('active-trail active');
+			// path.match(/^\/category\/blog\//) && $('li.menu-item-719').addClass('active-trail').find('a').addClass('active-trail active');
+			// path.match(/^\/gallery\//) && $('li.menu-item-1928').addClass('active-trail').find('a').addClass('active-trail active');
 		}
 	};
 })(jQuery);
